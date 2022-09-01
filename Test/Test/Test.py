34dcaf946a8ac41fb3659ee04ec9545017c3e740
ironwolftamerUCCS <bstampe2@uccs.edu>
2022-09-01 14:01:15 -0600

    Testing Matrices in Test

--- Test/Test/Test.py
+++ Test/Test/Test.py
@@ -1,5 +1,16 @@
+# Testing making manual Matrices
 A = [[1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]]
 
 print("A[1][2] =", A[1][2])
+
+# Testing making generated Matrices
+n = 2
+m = 5
+increment = 1
+val = [5] * n
+for x in range (n):
+    val[x] = [0 + increment] * m
+    increment = increment + 1
+print(val)
