ec0036632003890ee75d35ba6726dee71a6523b7
ironwolftamerUCCS <bstampe2@uccs.edu>
2022-09-01 14:47:36 -0600

    Frustration with Arrays

--- Test/Test/Test.py
+++ Test/Test/Test.py
@@ -1,16 +1,27 @@
 # Testing making manual Matrices
+from array import array
+
+
 A = [[1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]]
 
 print("A[1][2] =", A[1][2])
+print();
 
 # Testing making generated Matrices
-n = 2
-m = 5
-increment = 1
-val = [5] * n
+n = 5
+m = 7
+val = [0 + n] * n
 for x in range (n):
-    val[x] = [0 + increment] * m
-    increment = increment + 1
+    val[x] = [0] * m
 print(val)
+
+# Trying Mat1
+n1 = 5
+m1 = 7
+mat1 = array([0] * n * m)
+for i in range (n * m):
+    mat1[i] = [i]
+mat1.reshape(n, m)
+print(mat1)
