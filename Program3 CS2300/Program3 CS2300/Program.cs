4e898aa54792d78213b04ae2833e73cb71df5a5a
ironwolftamerUCCS <bstampe2@uccs.edu>
2022-11-01 22:31:31 -0600

    renamed and move program 3 turn in

--- Program3 CS2300/Program3 CS2300/Program.cs
+++ Program3 CS2300/Program3 CS2300/Program.cs
@@ -8,37 +8,46 @@ namespace Program3_CS2300
     {
         static void Main(string[] args)
         {
-            //Ask for and pass in the file name
-            Console.Write("File name: ");
-            string fileName = Console.ReadLine();
+            bool keepTesting = true;
 
-            //Ask which test to run on the file
-            Console.WriteLine("Which test would you like to run? (1 (ax=b), 2(eigen things), or 3(triangle things))");
-            int test = int.Parse(Console.ReadLine());
-            switch (test)
+            while (keepTesting)
             {
-                case 1:
-                    //Set A and B
-                    List<float[,]> AAndB = FileProcessor.ReadInAAndB(fileName);
-                    float[,] A = AAndB[0];
-                    float[,] B = AAndB[1];
-                    Console.WriteLine("------");
-                    MatrixManipulator.SolveMatrixEquation(A, B, true);
-                    break;
+                //Ask for and pass in the file name
+                Console.Write("File name: ");
+                string fileName = Console.ReadLine();
 
-                case 2:
-                    AAndB = FileProcessor.ReadInAAndB(fileName);
-                    A = AAndB[0];
-                    Console.WriteLine("------");
-                    MatrixManipulator.SolveEigenThings(A);
-                    break;
-                case 3:
-                    float[,] vertices = FileProcessor.ReadInTriangleVertices(fileName);
-                    Console.WriteLine("------");
-                    MatrixManipulator.SolveTriangleThings(vertices);
-                    break;
+                //Ask which test to run on the file
+                Console.WriteLine("Which test would you like to run? (1 (ax=b), 2(eigen things), or 3(triangle things))");
+                int test = int.Parse(Console.ReadLine());
+                switch (test)
+                {
+                    case 1:
+                        //Set A and B
+                        List<float[,]> AAndB = FileProcessor.ReadInAAndB(fileName);
+                        float[,] A = AAndB[0];
+                        float[,] B = AAndB[1];
+                        Console.WriteLine("------");
+                        MatrixManipulator.SolveMatrixEquation(A, B, true);
+                        break;
 
-                default: Console.WriteLine("Invalid answer"); break;
+                    case 2:
+                        AAndB = FileProcessor.ReadInAAndB(fileName);
+                        A = AAndB[0];
+                        Console.WriteLine("------");
+                        MatrixManipulator.SolveEigenThings(A);
+                        break;
+                    case 3:
+                        float[,] vertices = FileProcessor.ReadInTriangleVertices(fileName);
+                        Console.WriteLine("------");
+                        MatrixManipulator.SolveTriangleThings(vertices);
+                        break;
+
+                    default: Console.WriteLine("Invalid answer"); break;
+                }
+
+                Console.WriteLine("Keep testing? (1 = yes, 0 = no)");
+                int answer = int.Parse(Console.ReadLine());
+                if (answer == 0) { keepTesting = false; }
             }
         }
     }
